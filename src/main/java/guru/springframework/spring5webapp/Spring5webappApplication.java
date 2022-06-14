3fc0dcd860181a7f26dba488a9acd71f691cfd41
IgorGritsanchouk <igor.gritsanchouk@gmail.com>
2022-06-14 11:57:43 +0100

    Base app for simplified deployment.

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -2,12 +2,27 @@ package guru.springframework.spring5webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Spring5webappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5webappApplication.class, args);
+		System.out.println("... Spring Application for AWS deploy ...");
 	}
 
+	@GetMapping("/")
+	public String welcome(){
+		return "... AWS light deployment ... CTRL ALT to format.";
+	}
+
+	@GetMapping("/{name}")
+	public String byName(@PathVariable String name)
+	{
+		return "Hi "+ name +". Welcome to AWS.";
+	}
 }
